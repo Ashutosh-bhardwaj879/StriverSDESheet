9af036cf86ac16343b6ec762f7f262f87d31ecb8
Ashutosh-bhardwaj879 <ashutoshbhardwaj879@gmail.com>
2022-10-08 13:47:48 +0530

    searchIn2d matrix less optimized

--- DAY3_ARRAYS/searchIn2dMatrix.cpp
+++ DAY3_ARRAYS/searchIn2dMatrix.cpp
@@ -1,24 +1,26 @@
 /*
- *Search in a sorted 2D matrix
- //https://takeuforward.org/data-structure/search-in-a-sorted-2d-matrix/
-Problem Statement: Given an m*n 2D matrix and an integer, write a program to find if the given integer exists in the matrix.
+ Search in a row wise and column wise sorted matrix
+Difficulty Level : Medium
+Last Updated : 19 Sep, 2022
+Read
+Discuss
 
-Given matrix has the following properties:
+Given an n x n matrix and an integer x, find the position of x in the matrix if it is present. Otherwise, print “Element not found”. Every row and column of the matrix is sorted in increasing order. The designed algorithm should have linear time complexity
+Input: mat[4][4] = { {10, 20, 30, 40},  x = 29
+                               {15, 25, 35, 45},
+                               {27, 29, 37, 48},
+                             {32, 33, 39, 50}}
 
-Integers in each row are sorted from left to right.
-The first integer of each row is greater than the last integer of the previous row
-Example 1:
+Output: Found at (2, 1)
+Explanation: Element at (2,1) is 29
 
-Input: matrix =
-[[1,3,5,7],
- [10,11,16,20],
- [23,30,34,60]],
+Input : mat[4][4] = { {10, 20, 30, 40},   x = 100
+                                {15, 25, 35, 45},
+                               {27, 29, 37, 48},
+                              {32, 33, 39, 50}};
 
-target = 3
-
-Output: true
-
-Explanation: As the given integer(target) exists in the given 2D matrix, the function has returned true.
+Output: Element not found
+Explanation: Element 100 does not exist in the matrix
 */
 #include <bits/stdc++.h>
 using namespace std;
@@ -27,10 +29,10 @@ bool searchMatrix(vector<vector<int>> &matrix, int target)
 {
     int rows = matrix.size();
     int cols = matrix[0].size();
-    //start from  the pointer to last elemnt of 0th row
-    int i = 0;     // 0th row
+    // start from  the pointer to last elemnt of 0th row
+    int i = 0;        // 0th row
     int j = cols - 1; // last column
-    //everythign is sorted since we can use this algo
+    // everythign is sorted since we can use this algo
     while (i < rows && j >= 0)
     {
         if (matrix[i][j] == target)
@@ -41,11 +43,11 @@ bool searchMatrix(vector<vector<int>> &matrix, int target)
         }
         else if (matrix[i][j] > target)
         {
-            j--;//move left
+            j--; // move left
         }
         else
         {
-            i++;//move down
+            i++; // move down
         }
     }
     return false;
@@ -62,7 +64,7 @@ int main()
         {25, 29, 39, 50},
         {50, 60, 70, 80}};
     int target = 36;
-    bool result = searchMatrix(matrix,target);
+    bool result = searchMatrix(matrix, target);
     cout << endl;
     cout << boolalpha << result << endl;
     return 0;
